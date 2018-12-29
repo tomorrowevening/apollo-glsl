1848697fb5317682ef8d2c9b7aafe4ae4ed32874
tomorrowevening <colin@tomorrowevening.com>
2018-12-28 17:20:41 -0800

    grain added

--- common.glsl
+++ common.glsl
@@ -29,6 +29,19 @@ float random(vec2 st) {
       43758.5453123);
 }
 
+// Based on thebookofshaders
+// https://github.com/patriciogonzalezvivo/thebookofshaders/blob/master/18/grain.frag
+vec3 random3(vec3 c) {
+  float j = 4096.0*sin(dot(c,vec3(17.0, 59.4, 15.0)));
+  vec3 r;
+  r.z = fract(512.0*j);
+  j *= .125;
+  r.x = fract(512.0*j);
+  j *= .125;
+  r.y = fract(512.0*j);
+  return r-0.5;
+}
+
 // 2D Noise based on Morgan McGuire @morgan3d
 // https://www.shadertoy.com/view/4dS3Wd
 float noise(in vec2 st) {
@@ -51,6 +64,51 @@ float noise(in vec2 st) {
   return mix(a, b, u.x) +
           (c - a)* u.y * (1.0 - u.x) +
           (d - b) * u.x * u.y;
+}
+
+const float F3 =  0.3333333;
+const float G3 =  0.1666667;
+float snoise(vec3 p) {
+  vec3 s = floor(p + dot(p, vec3(F3)));
+  vec3 x = p - s + dot(s, vec3(G3));
+  vec3 e = step(vec3(0.0), x - x.yzx);
+  vec3 i1 = e*(1.0 - e.zxy);
+  vec3 i2 = 1.0 - e.zxy*(1.0 - e);
+  vec3 x1 = x - i1 + G3;
+  vec3 x2 = x - i2 + 2.0*G3;
+  vec3 x3 = x - 1.0 + 3.0*G3;
+  vec4 w, d;
+  w.x = dot(x, x);
+  w.y = dot(x1, x1);
+  w.z = dot(x2, x2);
+  w.w = dot(x3, x3);
+  w = max(0.6 - w, 0.0);
+  d.x = dot(random3(s), x);
+  d.y = dot(random3(s + i1), x1);
+  d.z = dot(random3(s + i2), x2);
+  d.w = dot(random3(s + 1.0), x3);
+  w *= w;
+  w *= w;
+  d *= w;
+  return dot(d, vec4(52.0));
+}
+
+//  Author: Matt DesLauriers
+//  https://github.com/mattdesl/glsl-film-grain/blob/master/index.glsl
+//
+float grain(vec2 texCoord, vec2 resolution, float frame, float multiplier) {
+  vec2 mult = texCoord * resolution;
+  float offset = snoise(vec3(mult / multiplier, frame));
+  float n1 = snoise(vec3(mult, offset));
+  return n1 / 2.0 + 0.5;
+}
+
+float grain(vec2 texCoord, vec2 resolution, float frame) {
+  return grain(texCoord, resolution, frame, 2.5);
+}
+
+float grain(vec2 texCoord, vec2 resolution) {
+  return grain(texCoord, resolution, 0.0);
 }
 
 //////////////////////////////////////////////////
@@ -175,7 +233,10 @@ float polygon(vec2 p, int vertices, float size) {
 #pragma glslify: export(getPixel)
 
 #pragma glslify: export(random)
+#pragma glslify: export(random3)
 #pragma glslify: export(noise)
+#pragma glslify: export(snoise)
+#pragma glslify: export(grain)
 
 #pragma glslify: export(rotate2D)
 #pragma glslify: export(scale2D)
